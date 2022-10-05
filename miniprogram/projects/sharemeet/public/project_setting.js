553dfdac8376e0b9bf8da7ce07455d35c95712fc
csuxiaofeizhao <csuxiaofeizhao@126.com>
2022-10-05 18:08:17 +0800

    CR

---
+++ miniprogram/projects/sharemeet/public/project_setting.js
@@ -0,0 +1,45 @@
+module.exports = { //共享会议室预定
+	PROJECT_COLOR: '#3879C2',
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#3879C2',
+
+
+	// setup
+	SETUP_CONTENT_ITEMS: [
+		{ title: '关于我们', key: 'SETUP_CONTENT_ABOUT' },
+	],
+
+	// 用户
+	USER_REG_CHECK: false,
+	USER_FIELDS: [
+		{ mark: 'department', title: '所在部门', type: 'text', must: false },
+	],
+
+	NEWS_NAME: '公告通知',
+	NEWS_CATE: [
+		{ id: 1, title: '公告通知', style: 'leftbig1' },
+
+	],
+	NEWS_FIELDS: [
+	],
+
+	ENROLL_NAME: '会议室',
+	ENROLL_CATE: [
+		{ id: 1, title: 'A区' },
+		{ id: 2, title: 'B区' },
+		{ id: 3, title: 'C区' },
+		{ id: 4, title: 'D区' },
+	],
+	ENROLL_FIELDS: [
+		{ mark: 'cover', title: '封面图片', type: 'image', min: 1, max: 1, must: true },
+		{ mark: 'person', title: '最大容纳人数', type: 'text', must: true },
+		{ mark: 'tools', title: '设施情况', type: 'checkbox', selectOptions: ['白板', '投影仪', '话筒', '音箱'], checkBoxLimit: 1, must: false },
+	],
+	ENROLL_JOIN_FIELDS: [
+		{ mark: 'name', type: 'text', title: '会议名称', must: true, max: 30, edit: false },
+		{ mark: 'desc', type: 'textarea', title: '会议描述', must: false, max: 100, edit: false },
+		{ mark: 'person', type: 'text', title: '预订人', must: true, max: 30, edit: false },
+		{ mark: 'tel', type: 'mobile', title: '联系电话', must: true, edit: false },
+	],
+
+}
